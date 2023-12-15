e450cabc9afd2cde7a5a1aa54873cdd07b679851
Alex Luo <theaxluo@gmail.com>
2023-12-15 17:20:01 -0600

    updated checkpoint test to be more in line with pytest standards

--- UnitTests/checkpoint_test.py
+++ UnitTests/checkpoint_test.py
@@ -2,24 +2,24 @@ import pytest
 import ray
 from Concurrency.storage import Storage
 
-@pytest.fixture(autouse=True)
-def setup(episode):
-    storage = Storage.remote(episode)
-    yield storage
+def setup_function(episode):
+    global storage
+    storage = Storage.remote(0)
+    # yield storage
 
-def store_checkpoint_test(storage):
+def test_store_checkpoint():
     storage.store_checkpoint.remote(1000000)
     checkpoint_list = ray.get(storage.get_checkpoint_list.remote())
     assert checkpoint_list[-1].epoch == 1000000
 
-def update_checkpoint_score_test(storage):
+def test_update_checkpoint_score():
     # First test is for a random checkpoint,
     storage.store_checkpoint.remote(123456)
     storage.update_checkpoint_score.remote(123456, 1)
     checkpoint_list = ray.get(storage.get_checkpoint_list.remote())
     assert checkpoint_list[-1].q_score < 1
 
-def sample_past_model_test(storage):
+def test_sample_past_model():
     model, choice, probability = ray.get(storage.sample_past_model.remote())
     assert model
     assert choice % 100 == 0
@@ -35,7 +35,6 @@ def update_q_score_test(storage):
 # Storage - store_checkpoint, update_checkpoint, update_checkpoint_score, sample_past_model, get_model
 # Checkpoint - get_model, update_q_score
 # def test_list():
-#     storage = setup(0)
 #     store_checkpoint_test(storage)
 #     update_checkpoint_score_test(storage)
 #     sample_past_model_test(storage)
