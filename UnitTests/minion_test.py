143cdab7983fc7b88685fab365028d26cae1a920
Alex Luo <theaxluo@gmail.com>
2024-01-03 12:16:42 -0600

    updated minion test to use pytest

--- UnitTests/minion_test.py
+++ UnitTests/minion_test.py
@@ -6,15 +6,15 @@ from Simulator import minion
 # contains the list of round numbers where unique PVE rounds occur
 rounds = [0,1,2,8,14,20,26,33]
 
-def setup() -> Player:
+def create_player() -> Player:
     """Creates fresh player and pool"""
     base_pool = pool()
     player1 = Player(base_pool, 0)
     return player1
 
 # Check health calculation from minion combat
-def combatTest():
-    p1 = setup()
+def test_combat():
+    p1 = create_player()
     p1.gold = 10000
     p1.max_units = 100
     minion.minion_combat(p1, minion.FirstMinion(), 0, [None for _ in range(2)])
@@ -22,8 +22,8 @@ def combatTest():
     assert p1.health < 100, "I didn't lose any health from losing a PVE round!"
 
 # test if each round is dropping rewards for the player
-def rewardsTest():
-    p1 = setup()
+def test_rewards():
+    p1 = create_player()
     # add 3* zilean and yone to board for combat
     p1.board[0][0] = champion("zilean", None, 0, 0, 3, None, None, None, False)
     p1.board[0][1] = champion("yone", None, 0, 0, 3, None, None, None, False)
@@ -36,16 +36,6 @@ def rewardsTest():
         
         minion.minion_round(p1, r, [None for _ in range(2)])
 
-        assert not emptyList(p1.item_bench) \
-            or not emptyList(p1.bench) \
+        assert not len(p1.item_bench) == 0 \
+            or len(p1.bench) == 0 \
             or not p1.gold == 0, "I didn't get anything from the PVE round!"
-
-def emptyList(listArr):
-    for i in listArr:
-        if i is not None:
-            return False
-    return True
-
-def test_list():
-    combatTest()
-    rewardsTest()
